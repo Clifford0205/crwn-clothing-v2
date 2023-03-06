00a192a37ea677209bdbcdd657e5638b79c70d82
Clifford Tien <ts02690140@gmail.com>
2023-03-06 11:11:16 +0800

    feat(category saga): saga call 參數講解

--- src/store/categories/category.saga.js
+++ src/store/categories/category.saga.js
@@ -11,6 +11,7 @@ import { CATEGORIES_ACTION_TYPES } from './category.types';
 
 export function* fetchCategoriesAsync() {
   try {
+    // call 第一個參數是function名稱 第二個參數以後的都是第一個function的參數
     const categoriesArray = yield call(getCategoriesAndDocuments, 'categories');
     yield put(fetchCategoriesSuccess(categoriesArray));
   } catch (error) {
